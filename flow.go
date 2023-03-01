2576c7b099fff89829f62861d907526aefba05fb
x <xxx>
2023-03-01 10:27:50 +0800

    fix: 删除flowjob的grpc处理

--- flow.go
+++ flow.go
@@ -125,7 +125,6 @@ func (c *Client) BatchCreateFlowTask(ctx context.Context, projectId string, crea
 	return nil
 }
 
-
 // QueryFlow Flow
 func (c *Client) QueryFlow(ctx context.Context, projectId string, query, result interface{}) error {
 	if projectId == "" {
@@ -242,66 +241,6 @@ func (c *Client) BatchCreateFlow(ctx context.Context, projectId string, createDa
 	}
 	return nil
 }
-
-
-func (c *Client) QueryFlowJob(ctx context.Context, projectId string, query, result interface{}) error {
-	if projectId == "" {
-		projectId = config.XRequestProjectDefault
-	}
-	bts, err := json.Marshal(query)
-	if err != nil {
-		return errors.NewMsg("序列化查询参数为空, %s", err)
-	}
-	cli, err := c.FlowClient.GetFlowJobServiceClient()
-	if err != nil {
-		return errors.NewMsg("获取客户端错误,%s", err)
-	}
-	res, err := cli.Query(
-		metadata.GetGrpcContext(ctx, map[string]string{config.XRequestProject: projectId}),
-		&api.QueryRequest{Query: bts})
-	if err != nil {
-		return errors.NewMsg("请求错误, %s", err)
-	}
-	if !res.GetStatus() {
-		return errors.NewErrorMsg(errors.NewMsg("响应不成功, %s", res.GetDetail()), res.GetInfo())
-	}
-	if err := json.Unmarshal(res.GetResult(), result); err != nil {
-		return errors.NewMsg("解析请求结果错误, %s", err)
-	}
-	return nil
-}
-
-func (c *Client) BatchCreateFlowJob(ctx context.Context, projectId string, createData, result interface{}) error {
-	if projectId == "" {
-		projectId = config.XRequestProjectDefault
-	}
-	if createData == nil {
-		return errors.NewMsg("插入数据为空")
-	}
-	cli, err := c.FlowClient.GetFlowJobServiceClient()
-	if err != nil {
-		return errors.NewMsg("获取客户端错误,%s", err)
-	}
-	bts, err := json.Marshal(createData)
-	if err != nil {
-		return errors.NewMsg("序列化插入数据为空")
-	}
-	res, err := cli.BatchCreate(
-		metadata.GetGrpcContext(ctx, map[string]string{config.XRequestProject: projectId}),
-		&api.CreateRequest{Data: bts})
-	if err != nil {
-		return errors.NewMsg("请求错误, %s", err)
-	}
-	if !res.GetStatus() {
-		return errors.NewErrorMsg(errors.NewMsg("响应不成功, %s", res.GetDetail()), res.GetInfo())
-	}
-	if err := json.Unmarshal(res.GetResult(), result); err != nil {
-		return errors.NewMsg("解析请求结果错误, %s", err)
-	}
-	return nil
-}
-
-
 
 // CreateFlowTriggerRecord FlowTriggerRecord
 func (c *Client) CreateFlowTriggerRecord(ctx context.Context, projectId string, createData, result interface{}) error {
