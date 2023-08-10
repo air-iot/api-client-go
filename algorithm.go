263f3aa60c7a02f0c0ed618e664d0fa0edfc3186
tbz <tianbaozhen@foxmail.com>
2023-08-10 11:00:09 +0800

    fix: 增加算法部分

---
+++ algorithm.go
@@ -0,0 +1,44 @@
+package api_client_go
+
+import (
+	"context"
+	"github.com/air-iot/api-client-go/v4/algorithm"
+	"github.com/air-iot/api-client-go/v4/apicontext"
+	"github.com/air-iot/api-client-go/v4/config"
+	"github.com/air-iot/api-client-go/v4/errors"
+	cErrors "github.com/air-iot/errors"
+	"github.com/air-iot/json"
+)
+
+// AlgorithmRun 算法执行
+func (c *Client) AlgorithmRun(ctx context.Context, projectId, key string, data interface{}) ([]byte, error) {
+	if projectId == "" {
+		projectId = config.XRequestProjectDefault
+	}
+	cli, err := c.AlgorithmClient.GetAlgorithmServiceClient()
+	if err != nil {
+		return nil, errors.NewMsg("获取客户端错误,%s", err)
+	}
+	if data == nil {
+		return nil, errors.NewMsg("数据为空")
+	}
+
+	bts, err := json.Marshal(data)
+	if err != nil {
+		return nil, errors.NewMsg("数据为空")
+	}
+	res, err := cli.Run(
+		apicontext.GetGrpcContext(ctx, map[string]string{config.XRequestProject: projectId}),
+		&algorithm.RunRequest{
+			Key:  key,
+			Data: bts,
+		})
+	if err != nil {
+		return nil, errors.NewMsg("请求错误, %s", err)
+	}
+	if !res.GetStatus() {
+		return nil, cErrors.Wrap400Response(err, int(res.GetCode()), "响应不成功, %s", res.GetDetail())
+	}
+
+	return res.GetResult(), nil
+}
