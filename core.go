3d83e3a86f8b16c6c68cee5643effdd455f1039f
zhangqiang <z397435052@gmail.com>
2023-09-05 16:26:51 +0800

    fix: 查询表记录返回count

--- core.go
+++ core.go
@@ -838,20 +838,20 @@ func (c *Client) CreateTableRecord(ctx context.Context, projectId string, create
 	return nil
 }
 
-func (c *Client) QueryTableData(ctx context.Context, projectId, tableName string, query, result interface{}) error {
+func (c *Client) QueryTableData(ctx context.Context, projectId, tableName string, query, result interface{}) (int64, error) {
 	if projectId == "" {
 		projectId = config.XRequestProjectDefault
 	}
 	if tableName == "" {
-		return errors.NewMsg("表为空")
+		return 0, errors.NewMsg("表为空")
 	}
 	cli, err := c.CoreClient.GetTableDataServiceClient()
 	if err != nil {
-		return errors.NewMsg("获取客户端错误,%s", err)
+		return 0, errors.NewMsg("获取客户端错误,%s", err)
 	}
 	bts, err := json.Marshal(query)
 	if err != nil {
-		return errors.NewMsg("序列化查询参数为空, %s", err)
+		return 0, errors.NewMsg("序列化查询参数为空, %s", err)
 	}
 	res, err := cli.Query(
 		apicontext.GetGrpcContext(ctx, map[string]string{config.XRequestProject: projectId}),
@@ -860,15 +860,17 @@ func (c *Client) QueryTableData(ctx context.Context, projectId, tableName string
 			Query: bts,
 		})
 	if err != nil {
-		return errors.NewMsg("请求错误, %s", err)
+		return 0, errors.NewMsg("请求错误, %s", err)
 	}
 	if !res.GetStatus() {
-		return errors.NewErrorMsg(errors.NewMsg("响应不成功, %s", res.GetDetail()), res.GetInfo())
+		return 0, errors.NewErrorMsg(errors.NewMsg("响应不成功, %s", res.GetDetail()), res.GetInfo())
 	}
-	if err := json.Unmarshal(res.GetResult(), result); err != nil {
-		return errors.NewMsg("解析请求结果错误, %s", err)
+	if res.GetResult() != nil {
+		if err := json.Unmarshal(res.GetResult(), result); err != nil {
+			return res.GetCount(), errors.NewMsg("解析请求结果错误, %s", err)
+		}
 	}
-	return nil
+	return res.GetCount(), nil
 }
 
 func (c *Client) QueryTableDataByTableId(ctx context.Context, projectId, tableId string, query, result interface{}) error {
