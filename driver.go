c88f4821b2b1ca7009f6ba86fb31c8940a4b2793
zhgqiang <z397435052@gmail.com>
2024-03-13 11:09:28 +0800

    fix: 增加写入数据点

--- driver.go
+++ driver.go
@@ -2,15 +2,31 @@ package api_client_go
 
 import (
 	"context"
-	"github.com/air-iot/api-client-go/v4/driver"
 
 	"github.com/air-iot/api-client-go/v4/api"
 	"github.com/air-iot/api-client-go/v4/apicontext"
 	"github.com/air-iot/api-client-go/v4/config"
+	"github.com/air-iot/api-client-go/v4/driver"
 	"github.com/air-iot/api-client-go/v4/errors"
 	cErrors "github.com/air-iot/errors"
 	"github.com/air-iot/json"
 )
+
+type DriverWriteTag struct {
+	Table     string      `json:"table"`
+	TableData string      `json:"tableData"`
+	ID        string      `json:"id" bson:"id"`
+	Params    interface{} `json:"params" bson:"params"`
+}
+
+type DriverBatchWriteTag struct {
+	TableId      string      `json:"tableId"`
+	TableDataIds []string    `json:"tableDataIds"`
+	ID           string      `json:"id" bson:"id"`
+	Query        string      `json:"query"`
+	Type         string      `json:"type"`
+	Params       interface{} `json:"params" bson:"params"`
+}
 
 // BatchCommand 批量执行指令
 func (c *Client) BatchCommand(ctx context.Context, projectId string, data interface{}, result interface{}) error {
@@ -65,6 +81,68 @@ func (c *Client) ChangeCommand(ctx context.Context, projectId, id string, data, 
 	res, err := cli.ChangeCommand(
 		apicontext.GetGrpcContext(ctx, map[string]string{config.XRequestProject: projectId}),
 		&api.UpdateRequest{Id: id, Data: bts})
+	if err != nil {
+		return errors.NewMsg("请求错误, %s", err)
+	}
+	if !res.GetStatus() {
+		return cErrors.Wrap400Response(err, int(res.GetCode()), "响应不成功, %s", res.GetDetail())
+	}
+	if err := json.Unmarshal(res.GetResult(), result); err != nil {
+		return errors.NewMsg("解析请求结果错误, %s", err)
+	}
+	return nil
+}
+
+// DriverWriteTag 执行写数据点
+func (c *Client) DriverWriteTag(ctx context.Context, projectId string, data *DriverWriteTag, result interface{}) error {
+	if projectId == "" {
+		projectId = config.XRequestProjectDefault
+	}
+	cli, err := c.DriverClient.GetDriverServiceClient()
+	if err != nil {
+		return errors.NewMsg("获取客户端错误,%s", err)
+	}
+	if data == nil {
+		return errors.NewMsg("更新数据为空")
+	}
+	bts, err := json.Marshal(data)
+	if err != nil {
+		return errors.NewMsg("marshal 更新数据为空")
+	}
+	res, err := cli.WriteTag(
+		apicontext.GetGrpcContext(ctx, map[string]string{config.XRequestProject: projectId}),
+		&api.CreateRequest{Data: bts})
+	if err != nil {
+		return errors.NewMsg("请求错误, %s", err)
+	}
+	if !res.GetStatus() {
+		return cErrors.Wrap400Response(err, int(res.GetCode()), "响应不成功, %s", res.GetDetail())
+	}
+	if err := json.Unmarshal(res.GetResult(), result); err != nil {
+		return errors.NewMsg("解析请求结果错误, %s", err)
+	}
+	return nil
+}
+
+// DriverBatchWriteTag 执行写数据点
+func (c *Client) DriverBatchWriteTag(ctx context.Context, projectId string, data *DriverBatchWriteTag, result interface{}) error {
+	if projectId == "" {
+		projectId = config.XRequestProjectDefault
+	}
+	cli, err := c.DriverClient.GetDriverServiceClient()
+	if err != nil {
+		return errors.NewMsg("获取客户端错误,%s", err)
+	}
+	if data == nil {
+		return errors.NewMsg("更新数据为空")
+	}
+	bts, err := json.Marshal(data)
+	if err != nil {
+		return errors.NewMsg("marshal 更新数据为空")
+	}
+	res, err := cli.BatchWriteTag(
+		apicontext.GetGrpcContext(ctx, map[string]string{config.XRequestProject: projectId}),
+		&api.CreateRequest{Data: bts})
 	if err != nil {
 		return errors.NewMsg("请求错误, %s", err)
 	}
