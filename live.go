dc12c2628c1babcde104ede42bcb975118e53233
tbz <tianbaozhen@foxmail.com>
2025-04-21 17:37:00 +0800

    fix: 增加live服务接口

--- live.go
+++ live.go
@@ -2,6 +2,7 @@ package api_client_go
 
 import (
 	"context"
+	"github.com/air-iot/api-client-go/v4/live"
 
 	"github.com/air-iot/api-client-go/v4/api"
 	"github.com/air-iot/api-client-go/v4/apicontext"
@@ -37,4 +38,42 @@ func (c *Client) RtspPull(ctx context.Context, projectId string, createData inte
 	} else {
 		return "", errors.New("result中path为空")
 	}
+}
+
+func (c *Client) LivePull2Ws(ctx context.Context, projectId string, createData interface{}) (string, error) {
+	return c.RtspPull(ctx, projectId, createData)
+}
+
+func (c *Client) LiveStreamsInfo(ctx context.Context, projectId string, result interface{}) error {
+	if projectId == "" {
+		projectId = config.XRequestProjectDefault
+	}
+	cli, err := c.LiveClient.GetLiveServiceClient()
+	if err != nil {
+		return err
+	}
+	res, err := cli.QueryStreamsInfo(
+		apicontext.GetGrpcContext(ctx, map[string]string{config.XRequestProject: projectId}),
+		&api.QueryRequest{Query: nil})
+	if _, err = parseRes(err, res, result); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c *Client) LiveStreamStopByStreamPath(ctx context.Context, projectId, streamPath string) error {
+	if projectId == "" {
+		projectId = config.XRequestProjectDefault
+	}
+	cli, err := c.LiveClient.GetLiveServiceClient()
+	if err != nil {
+		return err
+	}
+	res, err := cli.StopStreamByStreamPath(
+		apicontext.GetGrpcContext(ctx, map[string]string{config.XRequestProject: projectId}),
+		&live.LiveStopStreamByStreamPathRequest{StreamPath: streamPath})
+	if _, err = parseRes(err, res, nil); err != nil {
+		return err
+	}
+	return nil
 }
