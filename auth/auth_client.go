08080d9935abffc15602f3affea2056863b67693
zhgqiang <z397435052@163.com>
2025-04-28 16:40:15 +0800

    fix: token

--- auth/auth_client.go
+++ auth/auth_client.go
@@ -94,7 +94,7 @@ func (a *Client) Token() (token string, err error) {
 			return "", err
 		}
 	} else {
-		if a.authToken.ExpiresAt <= time.Now().Unix() {
+		if a.authToken.ExpiresAt <= (time.Now().Unix() - 120) {
 			authToken, err = a.getToken()
 			if err != nil {
 				return "", err
